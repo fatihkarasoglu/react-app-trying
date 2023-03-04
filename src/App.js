29d18a3f9c2f1ea2f371f5a666d3e8195fb9efba
Fatih <nordikvictor.01@gmail.com>
2023-03-04 03:23:48 +0300

    deneme

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const key = '48afb245d0fe10f13555b750e33fb5ba';
+  useEffect(() => {
+    async function getUser() {
+      try{
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${key}`);
+        console.log(response);
+      } catch(error) {
+        console.log(error);
+      }
+    }
+    getUser();
+  })
+  return (<div className="App">Merhaba</div>);
+}
+
+export default App;
